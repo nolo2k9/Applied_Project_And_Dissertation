82842d046e2162b2dd314bc6a1e02afb09dcf3cc
Keith Nolan <g00351932@gmit.ie>
2021-02-26 12:31:30 +0000

    Added buttons to show 3 blocks per page on frontend. Buttons can be pressed to see blocks not shown. This reduces clutter on the front- end

--- frontend/src/components/Blockchain.js
+++ frontend/src/components/Blockchain.js
@@ -1,27 +1,63 @@
 import React, { useState, useEffect } from 'react'
+import { Button } from 'react-bootstrap'
 import { BASE_URL } from '../config'
 import Block from './Block'
-/*
-    Fetch blockchain data
-*/
+
+//Amount of blocks shown on 1 page
+const RANGE = 3
+
+//Fetch blockchain data
 function Blockchain() {
   const [blockchain, setBlockchain] = useState([])
+  const [blockchainLength, setBlockchainLength] = useState(0)
 
+  const getBlockchainPage = ({ start, end }) => {
+    fetch(`${BASE_URL}/blockchain/range?start=${start}&end=${end}`)
+      .then(res => res.json())
+      .then(json => setBlockchain(json));
+  }
   useEffect(() => {
     //Fetch Endpoint
-    fetch(`${BASE_URL}/blockchain`)
-      .then((res) => res.json())
-      .then((json) => setBlockchain(json))
+    getBlockchainPage({ start: 0, end: RANGE })
+
+    fetch(`${BASE_URL}/blockchain/length`)
+      .then(res => res.json())
+      .then(json => setBlockchainLength(json))
   }, [])
+
+  const buttonNumbers = []
+  //loop until i is greater than the page range
+  for (let i = 0; i < blockchainLength / RANGE; i++) {
+    // add to array
+    buttonNumbers.push(i)
+  }
 
   return (
     <div className="Blockchain">
       <h3>Blockchain</h3>
       <div>
-        {blockchain.map(block => <Block key={block.hash} block={block} />)}
+        {blockchain.map((block) => (
+          <Block key={block.hash} block={block} />
+        ))}
+      </div>
+      <div>
+        {
+        buttonNumbers.map(number => {
+          const start = number * RANGE
+          const end = (number + 1) * RANGE
+          return (
+            <span
+              key={number}
+              onClick={() => getBlockchainPage({ start, end })}>
+              <Button size="sm" varient="danger">
+                {number + 1}
+              </Button>{' '}
+            </span>
+          )
+        })}
       </div>
     </div>
   )
 }
 
-export default Blockchain;
+export default Blockchain
