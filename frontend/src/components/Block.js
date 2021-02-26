4c4b7418beb8353ab428a16380a076834305ccde
Keith Nolan <g00351932@gmit.ie>
2021-02-26 09:53:54 +0000

    Added buttons to toggle transactions

--- frontend/src/components/Block.js
+++ frontend/src/components/Block.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import Reac, { useState } from 'react'
+import { Button } from 'react-bootstrap'
 import { MILLISECONDS_PYTHON } from '../config'
 import Transaction from './Transactions'
 
+function ToggleTransaction({ block }) {
+  //initially set to false
+  const [displayTransaction, setDisplayTransaction] = useState(false)
+  //pull data from block
+  const { data } = block;
+
+  const toggleDisplay = () => {
+      //get the opposite to whatever the current state is
+      setDisplayTransaction(!displayTransaction);
+  }
+
+  if (displayTransaction) {
+    return (
+      <div>
+        {data.map((transaction) => (
+          <div key={transaction.id}>
+            <hr />
+            <Transaction transaction={transaction} />
+          </div>
+        ))}
+        <br />
+        <Button
+        varient="danger"
+        size="sm"
+        onClick={toggleDisplay}>
+        Hide Transactions
+        </Button>
+      </div>
+    )
+  }
+  // Default behaviour
+  return (
+    <div>
+      <br />
+      <Button varient="danger"
+       size="sm"
+       onClick={toggleDisplay}>Show Transactions</Button>
+    </div>
+  )
+}
+
 function Block({ block }) {
-  const { timestamp, hash, data } = block
+  const { timestamp, hash} = block
   //Substring of current hash
   const hashDisplay = `${hash.substring(0, 15)}...`
   //Display current timestamp
@@ -13,17 +55,10 @@ function Block({ block }) {
 
   return (
     <div className="Block">
-        <b>Block Stats</b>
+      <b>Block Stats</b>
       <div>Hash: {hashDisplay}</div>
       <div>Timestamp: {timestampDisplay}</div>
-      <div>
-        {data.map((transaction) => (
-          <div key={transaction.id}>
-            <hr />
-            <Transaction transaction={transaction} />
-          </div>
-        ))}
-      </div>
+      <ToggleTransaction block={block}/>
     </div>
   )
 }
